f5e38f2aca5ae433e29f9914887a14042f7d3726
Wilson Martinez <wilsonmb89@hotmail.com>
2020-11-24 11:25:56 -0500

    Cambio en la ruta del sw

--- js/app.js
+++ js/app.js
@@ -1,7 +1,13 @@
 
 
+var url = window.location.href;
+var swLocation = '/udemy-pwa-06-twittor/sw.js';
+
 if (navigator.serviceWorker) {
-    navigator.serviceWorker.register('/sw.js');
+    if (url.includes('localhost')) {
+        swLocation = '/sw.js'
+    }
+    navigator.serviceWorker.register(swLocation);
 }
 
 // Referencias de jQuery
